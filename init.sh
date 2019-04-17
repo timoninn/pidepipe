e4964937e646eff792786682abb9dbdd926ad837
Nikolai Timonin <nt@mobileup.ru>
2019-04-17 11:46:37 +0300

    Add project struct init script

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir -p data/{raw,interim/{train,dev,test},processed/{train,dev,test},submissions}
+mkdir -p srs/{data,features,models,visualization}
+mkdir -p models
+mkdir -p notebooks/{exploratory,reports}
+mkdir -p reporst/figures
+
+
+
