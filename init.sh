34763690008f2d764f68dc050a5905a37ec8d1a3
Nikolai Timonin <nt@mobileup.ru>
2019-05-22 16:51:22 +0300

    Create references folder on init script

--- init.sh
+++ init.sh
@@ -7,4 +7,5 @@ mkdir -p data/{raw,interim/{train,dev,test},processed/{train,dev,test},submissio
 mkdir -p srs/{data,features,models,visualization}
 mkdir -p models
 mkdir -p notebooks/{exploratory,reports}
+mkdir -p references
 mkdir -p reports/figures
