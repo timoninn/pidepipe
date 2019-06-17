8aa96c54fefb3a52046429b311249f5400e51799
Nikolai Timonin <nt@mobileup.ru>
2019-06-17 17:47:56 +0300

    Unzip file in folder if > 1 files

--- kaggle/competitions.py
+++ kaggle/competitions.py
@@ -9,7 +9,7 @@ DOWNLOAD = 'download'
 SUBMIT = 'submit'
 
 
-def downaload(comp_name, path=None):
+def downaload(comp_name, path=None, extractall=True):
 
     args = [
         KAGGLE,
@@ -23,7 +23,8 @@ def downaload(comp_name, path=None):
 
     subprocess.run(args)
 
-    _extract_all_zip(path)
+    if extractall:
+        _extract_all_zip(path)
 
 
 def _extract_all_zip(directory_path):
@@ -31,14 +32,19 @@ def _extract_all_zip(directory_path):
 
     for filename in os.listdir(path):
         if filename.endswith('.zip'):
-            _extract_zip(path, filename)
+            _extract_zip(path, filename, '.zip')
 
 
-def _extract_zip(path, filename):
+def _extract_zip(path, filename, ext):
     print('Unzip file %s' % filename)
 
     zip = zipfile.ZipFile(path / filename)
-    zip.extractall(path)
+
+    if len(zip.namelist()) > 1:
+        zip.extractall(path / filename.replace(ext,''))
+    else:
+        zip.extractall(path)
+
     zip.close()
 
     os.remove(path / filename)
