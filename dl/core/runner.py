97678efa34dd7cd727648a644e3ade336f54b1cb
Nikolai Timonin <nikki.timonin@gmail.com>
2019-08-21 00:35:19 +0300

    Update runner

--- dl/core/runner.py
+++ dl/core/runner.py
@@ -8,6 +8,7 @@ import numpy as np
 
 from callback import Callback
 from state import State
+from ..utils.functions import get_available_device
 
 Scheduler = _LRScheduler
 
@@ -16,21 +17,80 @@ class Runner():
 
     def __init__(
         self,
-        device=None
+        device: torch.device = None
     ):
-        self.device = device
+        self.device = device if device is not None else get_available_device()
+
         self.best_valid_loss = float('inf')
 
         self.callbacks: [Callback] = None
         self.state: State = None
 
-    def _run_event(self, name: str):
+    def _run_epoch(self, epoch):
 
-        if self.callbacks is not None:
-            for callback in self.callbacks:
-                getattr(callback, f'on_{name}')(self.state)
+        self._run_event('epoch_begin')
+
+        self._run_train_phase(epoch)
+        self._run_valid_phase(epoch)
+
+        self._run_event('epoch_end')
+
+    def _run_train_phase(self, epoch: int):
+
+        self._run_event('phase_begin')
+
+        print(f'{epoch}/{num_epochs} Epoch {epoch} (train)')
+
+        self.model.train()
+
+        num_batches = len(self.train_loader)
+        tk = tqdm(self.train_loader, total=num_batches)
+
+        running_loss = 0.0
+
+        for itr, batch in enumerate(tk):
+            loss = self._run_train_batch(batch)
+            running_loss += loss
+            tk.set_postfix({'loss': running_loss / (itr + 1)})
+
+        epoch_loss = running_loss / num_batches
+        print(f'Loss: {epoch_loss:.4}')
+
+        self._save_state(epoch, epoch_loss, 'last.pth')
+
+        self._run_event('phase_end')
+
+    def _run_valid_phase(self, epoch):
+
+        self._run_event('phase_begin')
+
+        print(f'{epoch}/{num_epochs} Epoch {epoch} (valid)')
+        with torch.no_grad():
+            self.model.eval()
+
+            num_batches = len(self.valid_loader)
+            tk = tqdm(self.valid_loader, total=num_batches)
+
+            running_loss = 0.0
+            for itr, batch in enumerate(tk):
+                loss = self._run_valid_batch(batch)
+                running_loss += loss
+
+        epoch_loss = running_loss / num_batches
+        print(f'Loss: {epoch_loss:.4}')
+
+        if epoch_loss < self.best_valid_loss:
+            print('New optimal found. Saving state')
+            self.best_valid_loss = epoch_loss
+
+            self._save_state(epoch, epoch_loss, 'best.pth')
+
+        self._run_event('phase_end')
 
     def _run_train_batch(self, batch):
+
+        self._run_event('batch_begin')
+
         images, masks = batch
 
         self.optimizer.zero_grad()
@@ -39,23 +99,46 @@ class Runner():
         masks = masks.to(self.device)
 
         outputs = self.model(images)
-
         loss = self.criterion(outputs, masks)
-
         loss.backward()
-
         self.optimizer.step()
 
-        return loss.intem()
+        self._run_event('batch_end')
 
-    def _run_valid_batch(self):
-        pass
+        return loss.item()
 
-    def _run_epoch(self):
-        pass
+    def _run_valid_batch(self, batch):
 
-    def _run_phase(self):
-        pass
+        self._run_event('batch_begin')
+
+        images, masks = batch
+
+        images = images.to(self.device)
+        masks = masks.to(self.device)
+
+        outputs = self.model(images)
+        loss = self.criterion(outputs, masks)
+
+        self._run_event('batch_end')
+
+        return loss.item()
+
+    def _run_event(self, name: str):
+
+        if self.callbacks is not None:
+            for callback in self.callbacks:
+                getattr(callback, f'on_{name}')(self.state)
+
+    def _save_state(self, epoch: int, epoch_loss: float, name: str):
+
+        state = {
+            "epoch": epoch,
+            "loss": epoch_loss,
+            "model_state_dict": self.model.state_dict(),
+            "optimizer_state_dict": self.optimizer.state_dict(),
+        }
+
+        torch.save(state, self.log_dir + name)
 
     def train(
         self,
@@ -77,8 +160,8 @@ class Runner():
 
         callbacks: [Callback]
     ):
-
-
+        self.train_loader = train_loader
+        self.valid_loader = valid_loader
 
         self.model = model
         self.callbacks = callbacks
@@ -86,102 +169,12 @@ class Runner():
         self.metrics = metrics
         self.optimizer = optimizer
 
+        self.log_dir = log_dir
 
         model.to(self.device)
 
         for epoch in range(num_epochs):
-
-            # One batch
-
-            # Train phase
-
-            self._run_event('epoch_begin')
-
-            self._run_event('phase_begin')
-
-            print(f'{epoch}/{num_epochs} Epoch {epoch} (train)')
-
-            model.train()
-            train_loader = loaders['train']
-
-            num_batches = len(train_loader)
-            tk = tqdm(train_loader, total=num_batches)
-
-            # Init metric/loss
-            running_loss = 0.0
-
-            for itr, batch in enumerate(tk):
-                self._run_event('batch_begin')
-
-                loss = self._run_train_batch(batch)
-
-                running_loss += loss
-
-                self._run_event('batch_end')
-                tk.set_postfix({'loss': running_loss / (itr + 1)})
-
-            # Train phase loss.
-
-            # Get current metric/loss value
-            epoch_loss = running_loss / num_batches
-            print(f'Loss: {epoch_loss:.4}')
-
-            state = {
-                "epoch": epoch,
-                "loss": epoch_loss,
-                "model_state_dict": model.state_dict(),
-                "optimizer_state_dict": optimizer.state_dict(),
-            }
-
-            torch.save(state, log_dir + 'last.pth')
-
-            self._run_event('phase_end')
-
-            # Valid pahse
-
-            self._run_event('phase_begin')
-
-            print(f'{epoch}/{num_epochs} Epoch {epoch} (valid)')
-            with torch.no_grad():
-                model.eval()
-                valid_loader = loaders['valid']
-
-                num_batches = len(valid_loader)
-                tk = tqdm(valid_loader, total=num_batches)
-
-                running_loss = 0.0
-
-                for itr, batch in enumerate(tk):
-                    self._run_event('batch_begin')
-
-                    # Valid phase one batch
-
-                    images, masks = batch
-
-                    images = images.to(self.device)
-                    masks = masks.to(self.device)
-
-                    outputs = model(images)
-
-                    loss = criterion(outputs, masks)
-
-                    running_loss += loss.item()
-
-                    self._run_event('batch_end')
-
-            # Valid phase loss.
-            epoch_loss = running_loss / num_batches
-            print(f'Loss: {epoch_loss:.4}')
-
-            if epoch_loss < self.best_valid_loss:
-                print('New optimal found. Saving state')
-                self.best_valid_loss = epoch_loss
-
-                torch.save(state, log_dir + 'best.pth')
-
-            self._run_event('phase_end')
-
-            self._run_event('epoch_end')
+            self._run_epoch(epoch)
 
     def evaluate(
         self,
--- dl/utils/functions.py
+++ dl/utils/functions.py
@@ -1,4 +1,10 @@
 from torch import nn
+import torch
+
+
+def get_available_device() -> torch.device:
+
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def get_activation_func(name: str == 'none'):
