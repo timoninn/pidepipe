60ef7dc7e356237971b00a947408d40776398952
Nikolai Timonin <nikki.timonin@gmail.com>
2019-09-04 00:50:50 +0300

    Add BCEDiceLoss

--- dl/utils/losses.py
+++ dl/utils/losses.py
@@ -1,7 +1,9 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
-from .functions import dice
+
+from .functions import dice, get_activation_func
 
 
 class DiceLoss(nn.Module):
@@ -30,6 +32,29 @@ class DiceLoss(nn.Module):
             activation=self.activation,
             reduction=self.reduction
         )
+
+class BCEDiceLoss(nn.Module):
+
+    def __init__(
+        self,
+        activation: str = 'sigmoid'
+    ):
+        super().__init__()
+
+        self.bce = nn.BCELoss()
+        self.dice = DiceLoss(activation=activation)
+        self.activation = get_activation_func(activation)
+
+    def forward(
+        self,
+        logits: torch.Tensor,
+        target: torch.Tensor
+    ) -> float:
+
+        predicted = self.activation(input=logits)
+
+        return self.bce(predicted, target) - torch.log(self.dice(logits, target))
+
 
 
 class BCEDiceWeightedLoss(nn.Module):
