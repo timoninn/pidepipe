7d321ecbc03aee1c8585044f2b586b9d5a2b0dd5
Nikolai Timonin <nikki.timonin@gmail.com>
2019-09-06 01:22:11 +0300

    Add early stopping callback

--- dl/callbacks/early_stopping.py
+++ dl/callbacks/early_stopping.py
@@ -1,5 +1,6 @@
 from ..core.callback import Callback
 from ..core.state import State
+from ..core.meter import Monitor
 
 
 class EarlyStoppingCallback(Callback):
@@ -7,6 +8,43 @@ class EarlyStoppingCallback(Callback):
     def __init__(
         self,
         monitor: str = 'val_loss',
-        patience: int = 3
+        minimize: bool = True,
+        patience: int = 3,
+        delta: float = 1e-6
     ):
-        pass
+        assert delta >= 0, 'Delta should be greater than or equal to zero'
+        assert patience > 0, 'Patience should be greater than zero'
+
+        self.monitor = Monitor(monitor)
+        self.minimize = minimize
+        self.patience = patience
+        self.delta = delta
+
+        self.best_value = float('+inf') if minimize else float('-inf')
+        self.num_fails = 0
+
+    def _check_best_value(self, value: float) -> bool:
+        if self.minimize:
+            return (self.best_value - value) >= self.delta
+        else:
+            return (value - self.best_value) >= self.delta
+
+    def _check_early_stopping(self, state: State):
+        value = state.meter.get_last_epoch_value(
+            phase=self.monitor.phase,
+            metric_name=self.monitor.metric_name
+        )
+
+        if self._check_best_value(value):
+            self.num_fails = 0
+            self.best_value = value
+        else:
+            self.num_fails += 1
+
+        if self.num_fails == self.patience:
+            print('Early stopping')
+            state.stop_train = True
+
+    def on_epoch_end(self, state: State):
+        if state.is_train_phase:
+            self._check_early_stopping(state)
