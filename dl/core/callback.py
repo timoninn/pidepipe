ff7d5fdc57ba931e7b4fcb194823484d4073df39
Nikolai Timonin <nt@mobileup.ru>
2019-09-09 20:06:05 +0300

    Re-organize event positions

--- dl/core/callback.py
+++ dl/core/callback.py
@@ -8,23 +8,23 @@ class Callback:
     def on_begin(self, state: State):
         pass
 
-    def on_end(self, state: State):
-        pass
-
     def on_epoch_begin(self, state: State):
         pass
 
-    def on_epoch_end(self, state: State):
-        pass
-
     def on_phase_begin(self, state: State):
-        pass
-
-    def on_phase_end(self, state: State):
         pass
 
     def on_batch_begin(self, state: State):
         pass
 
     def on_batch_end(self, state: State):
+        pass
+
+    def on_phase_end(self, state: State):
+        pass
+
+    def on_epoch_end(self, state: State):
+        pass
+
+    def on_end(self, state: State):
         pass
