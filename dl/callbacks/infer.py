a2ccd511d02a5cff724bcd8801c98d277f902c2b
Nikolai Timonin <nt@mobileup.ru>
2019-09-09 20:06:43 +0300

    Add files infer callback

--- dl/callbacks/infer.py
+++ dl/callbacks/infer.py
@@ -14,23 +14,30 @@ class InferCallback(Callback):
         out_dir: str
     ):
         self.out_dir = Path(out_dir)
+        self.out_dir.mkdir(parents=True, exist_ok=True)
+
         self.predictions = []
-
-    def on_epoch_begin(self, state: State):
-        pass
-
-    def on_epoch_end(self, state: State):
-        pass
-
-    def on_phase_begin(self, state: State):
-        pass
-
-    def on_phase_end(self, state: State):
-        np.save(self.out_dir / 'infer.npy', self.predictions)
-
-    def on_batch_begin(self, state: State):
-        pass
 
     def on_batch_end(self, state: State):
         for arr in state.output.cpu().numpy():
             self.predictions.append(arr)
+
+    def on_phase_end(self, state: State):
+        np.save(self.out_dir / 'infer.npy', self.predictions)
+
+
+class FilesInferCallback(Callback):
+
+    def __init__(
+        self,
+        out_dir: str
+    ):
+        self.out_dir = Path(out_dir)
+        self.out_dir.mkdir(parents=True, exist_ok=True)
+
+    def on_batch_end(self, state: State):
+        output = state.output.cpu().numpy()
+        ids = state.batch[3]
+
+        for id, out in zip(ids, output):
+            np.save(self.out_dir / f'{id}.npy', out)
