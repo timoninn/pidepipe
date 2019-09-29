937760714103111bc264e2c56750d823ff6b9371
Nikolai Timonin <nikki.timonin@gmail.com>
2019-09-30 01:22:00 +0300

    Add load functions to base model

--- dl/utils/model.py
+++ dl/utils/model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -5,3 +6,10 @@ class Model(nn.Module):
 
     def trainable_parameters(self):
         return self.parameters()
+
+    def load_from_path(self, checkpoint_path: str, key: str = 'model_state_dict'):
+        checkpoint = torch.load(checkpoint_path)
+        self.load(checkpoint, key)
+
+    def load(self, checkpoint, key: str = 'model_state_dict'):
+        self.load_state_dict(checkpoint[key])
